c14119e8f4ee1affa2f47cb285d5a155d44be46c
AliHussein2 <aloosh1990xoxobnm@gmail.com>
2023-06-22 12:33:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alsirat/Front.kt
@@ -0,0 +1,15 @@
+package com.example.alsirat
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.content.res.Resources
+
+class Front : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ly_front)
+
+
+}}
